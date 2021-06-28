86cd3944d798b5eb6d39adc7c9a5408af701f090
devid100 <82916614+devid100@users.noreply.github.com>
2021-06-28 23:01:28 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -179,7 +179,7 @@ io.popen("mkdir File_Bot")
 io.popen("cd File_Bot && rm -rf commands.lua.1") 
 io.popen("cd File_Bot && rm -rf commands.lua.2") 
 io.popen("cd File_Bot && rm -rf commands.lua.3") 
-io.popen("cd File_Bot && wget https://raw.githubusercontent.com/devid100/vvvvvv/main/File_Bot/commands.lua") 
+io.popen("cd File_Bot && wget https://raw.githubusercontent.com/devid100/lilililil/main/File_Bot/commands.lua") 
 t = "\27[35m".."\nAll Files Started : \n____________________\n"..'\27[m'
 i = 0
 for v in io.popen('ls File_Bot'):lines() do
@@ -1118,7 +1118,7 @@ end
 
 if text == 'تحديث السورس ' and DevSoFi(msg) then 
 os.execute('rm -rf DRAGON.lua')
-os.execute('wget https://raw.githubusercontent.com/devid100/vvvvvv/main/DRAGON.lua')
+os.execute('wget https://raw.githubusercontent.com/devid100/lilililil/main/DRAGON.lua')
 send(msg.chat_id_, msg.id_,'◉ تم تحديث السورس')
 dofile('DRAGON.lua')  
 end
@@ -2492,9 +2492,9 @@ end
 return false
 end
 os.execute('rm -rf DRAGON.lua')
-os.execute('wget https://raw.githubusercontent.com/devid100/vvvvvv/main/DRAGON.lua')
-os.execute('wget https://raw.githubusercontent.com/devid100/vvvvvv/main/library')
-os.execute('wget https://raw.githubusercontent.com/devid100/vvvvvv/main/File_Bot')
+os.execute('wget https://raw.githubusercontent.com/devid100/lilililil/main/DRAGON.lua')
+os.execute('wget https://raw.githubusercontent.com/devid100/lilililil/main/library')
+os.execute('wget https://raw.githubusercontent.com/devid100/lilililil/main/File_Bot')
 send(msg.chat_id_, msg.id_,' ◉ تم تحديث السورس')
 dofile('DRAGON.lua')  
 end
@@ -4452,7 +4452,7 @@ send(msg.chat_id_, msg.id_,t)
 end
 if text == "متجر الملفات" or text == 'المتجر' then
 if DevSoFi(msg) then
-local Get_Files, res = https.request("https://raw.githubusercontent.com/devid100/vvvvvv/main/getfile.json")
+local Get_Files, res = https.request("https://raw.githubusercontent.com/devid100/lilililil/main/getfile.json")
 if res == 200 then
 local Get_info, res = pcall(JSON.decode,Get_Files);
 vardump(res.plugins_)
@@ -4490,7 +4490,7 @@ t = " ◉ الملف ← "..file.."\n ◉ تم تعطيل ملف \n"
 else
 t = " ◉ بالتاكيد تم تعطيل ملف → "..file.."\n"
 end
-local json_file, res = https.request("https://raw.githubusercontent.com/devid100/vvvvvv/main/File_Bot/"..file)
+local json_file, res = https.request("https://raw.githubusercontent.com/devid100/lilililil/main/File_Bot/"..file)
 if res == 200 then
 os.execute("rm -fr File_Bot/"..file)
 send(msg.chat_id_, msg.id_,t) 
@@ -4510,7 +4510,7 @@ t = " ◉ بالتاكيد تم تفعيل ملف → "..file.." \n"
 else
 t = " ◉ الملف ← "..file.."\n ◉ تم تفعيل ملف \n"
 end
-local json_file, res = https.request("https://raw.githubusercontent.com/devid100/vvvvvv/main/File_Bot/"..file)
+local json_file, res = https.request("https://raw.githubusercontent.com/devid100/lilililil/main/File_Bot/"..file)
 if res == 200 then
 local chek = io.open("File_Bot/"..file,'w+')
 chek:write(json_file)
